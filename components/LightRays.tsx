2e6f3128208e7c5995c2133727f49e25bacde2a0
Riya p <rpateld.076@gmail.com>
2026-01-06 14:42:19 +0530

    Initial commit

--- components/LightRays.tsx
+++ components/LightRays.tsx
@@ -3,7 +3,6 @@
 import { useRef, useEffect, useState } from "react";
 import { Renderer, Program, Triangle, Mesh } from "ogl";
 
-
 export type RaysOrigin =
     | "top-center"
     | "top-center-offset"
